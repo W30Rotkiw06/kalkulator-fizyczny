adf214bd394d39f2039f04437f54c11463e976e4
wiktor_wieczorek@onet.pl <wiktor_wieczorek@onet.pl>
2024-12-28 17:25:50 +0000

    Created grawitacja and ServerModule1; Edited settings; Deleted Form1

---
+++ client_code/grawitacja/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import grawitacjaTemplate
+from anvil import *
+import anvil.server
+
+
+class grawitacja(grawitacjaTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def calculate_click(self, **event_args):
+    m1 = self.m1.text * pow(10, self.m1_scinot.text)
+    m2 = self.m2.text * pow(10, self.m2_scinot.text)
+    r = self.r.text * pow(10, self.r_scinot.text)
+    g = 6.61 * pow(10, -11)
+
+    gravity = (g *m1 *m2) / (r*r)
+    self.result.text = f"Result: {round(gravity, 3)} N"
+    
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,5 @@
+import anvil.server
+@anvil.server.callable
+
+def calculate(m1, m2, r):
+  pass
