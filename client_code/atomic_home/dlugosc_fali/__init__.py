790c7f76401a45231c1cc3ff0794d22d0313b42f
wiktor_wieczorek@onet.pl <wiktor_wieczorek@onet.pl>
2024-12-30 16:06:19 +0000

    Created hydrogen_abs.png and hydrogen_em.png; Edited atomic_home, atomic_home.dlugosc_fali, atomic_home.energia_foton, gravity_home.predkoscKosmiczna and 1 more; Deleted hydrogen.png

--- client_code/atomic_home/dlugosc_fali/__init__.py
+++ client_code/atomic_home/dlugosc_fali/__init__.py
@@ -7,11 +7,14 @@ class dlugosc_fali(dlugosc_faliTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.lenght_or_freq = "długości"
+    self.emmited_or_absorbed = "emitowanego"
+    
 
     # Any code you write here will run before the form opens.
 
   def calculate_click(self, **event_args):
-    if (self.m.text >0 and self.n.text >0 ):
+    if (self.m.text >0 and self.n.text >0 and self.m.text != self.n.text):
       self.data_choose_change()
       result = anvil.server.call("calculate_wave", self.m.text, self.n.text, self.is_lambda)
       self.result.text = f"Result: {result} {self.unit}"
@@ -20,16 +23,19 @@ class dlugosc_fali(dlugosc_faliTemplate):
 
 
   def data_choose_change(self, **event_args):
-    # λ or f
-    
+    # λ or f    
     if self.data_choose.selected_value == "λ":
       self.is_lambda = True
-      self.title.text = "Kalkulator długości emitowanego fotonu przez wodór"
+      self.lenght_or_freq = "długości"
       self.unit = "m"
     else: 
       self.is_lambda = False
-      self.title.text = "Kalkulator częstotliwości emitowanego fotonu przez wodór"
+      self.lenght_or_freq = "częstotliwości"
       self.unit = "Hz"
+      
+    self.title.text = f"Kalkulator {self.lenght_or_freq} {self.emmited_or_absorbed} fotonu przez wodór"
+
+    
 
   def head_button_click(self, **event_args):
     open_form("home")
@@ -44,3 +50,18 @@ class dlugosc_fali(dlugosc_faliTemplate):
   def to_atom_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     open_form("atomic_home")
+
+
+
+  def m_n_change(self, **event_args):
+    print("change", self.m.text, self.n.text )
+    if (self.m.text is not None and self.n.text is not None):
+      if self.m.text > self.n.text: 
+        self.emmited_or_absorbed = "absorbowanego"
+        self.hydrogen_image.source = "_/theme/hydrogen_abs.png"
+      else:
+        self.emmited_or_absorbed = "emitowanego"
+        self.hydrogen_image.source = "_/theme/hydrogen_em.png"
+    self.title.text = f"Kalkulator {self.lenght_or_freq} {self.emmited_or_absorbed} fotonu przez wodór"
+
+      
